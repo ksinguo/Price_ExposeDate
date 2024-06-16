afd7bf01d327ebc62434701aea55599962f3f859
sin <kbsitmal@gmail.com>
2024-06-16 16:11:04 +0800

    修改app.js中的views路徑

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var engine = require('ejs-locals');
 var bodyParser = require('body-parser');
+const path = require('path');
 //載入firebase
 var admin = require("firebase-admin");
 //var serviceAccount = require("./test-352ee-firebase-adminsdk-jlepp-ba24cca1c6.json");
@@ -28,7 +29,8 @@ admin.initializeApp({
 
 
 app.engine('ejs',engine);
-app.set('views','./views');
+//app.set('views','./views');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','ejs');
 //增加靜態檔案的路徑，之後如果要用到public靜態文件，路徑寫法是/js/all.js，如果沒有寫是沒辦法抓到public裡面的檔案
 app.use(express.static('public'))
